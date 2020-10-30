b25433367ba1628f9eec35a996dc26e5062ace3a
Danish-Asghar2909 <danish.2909@icloud.com>
2020-10-30 05:13:29 -0700

    hello

--- server.js
+++ server.js
@@ -9,16 +9,15 @@ app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json());
 app.use(cors());
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
  const db = knex({
   client: 'pg',
   connection: {
-    
-      connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'danish0786',
+    database : 'smartbrain'
   }
-}
 });
 
 
